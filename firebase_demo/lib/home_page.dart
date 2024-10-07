ce816cbef658def41e037b1bbc95322614ec7a10
JGrisby2 <Jayden.Grisby@gmail.com>
2024-10-07 16:52:40 -0500

    Added Chat Messages

--- firebase_demo/lib/home_page.dart
+++ firebase_demo/lib/home_page.dart
@@ -5,11 +5,13 @@
 import 'package:firebase_auth/firebase_auth.dart' // new
     hide EmailAuthProvider, PhoneAuthProvider;    // new
 import 'package:flutter/material.dart';           // new
+import 'package:gtk_flutter/guest_book.dart';
 import 'package:provider/provider.dart';          // new
 
 import 'app_state.dart';                          // new
 import 'src/authentication.dart';                 // new
 import 'src/widgets.dart';
+import 'guest_book.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -46,6 +48,22 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
+          ),
+          const Header('Discussion'),
+          GuestBook(addMessage: (message) => print(message)),
         ],
       ),
     );
